326a4cd2b746a9cf40314113397a5d0aa6f9ce6d
Matthew Hrydil <mhrydil@gmail.com>
2020-09-23 12:17:27 -0400

    Rename polkadot_apps_lecture to experimental_code (I think I did this in a bad way because it deleted all the old contents and re-added it... oops). Add meetings folder with markdown of today's meeting. Add show_new_heads_parachains.js which shows updates to the heads of the parachains connected to rococo.

--- experimental_code/track_account.js
+++ experimental_code/track_account.js
@@ -9,34 +9,30 @@ const{ ApiPromise, WsProvider } = require('@polkadot/api');
     const api = await ApiPromise.create({ provider })
     const chain = await api.rpc.system.chain();
 
+    console.log(api);
+
     console.log(`Connected to ${chain}!`);
 
 
     let blockCount = 0;
+
     function showDataAboutBlock(block){
         console.log(chain + ": last block: " + block.number + " with hash: " + block.hash);
-        if (++blockCount == 5){
+        if (++blockCount == 50){
             blockUnsub();
+
         }
     }
+
     const blockUnsub = await api.rpc.chain.subscribeNewHeads(showDataAboutBlock);
 
-    // const blockSub = await api.rpc.chain.subscribeNewHeads((lastHeader) => {
-    //     console.log(chain + ": Last block: " + lastHeader.number + " with hash: " + lastHeader.hash);
-    // });
 
 
-    let balanceChangeCount = 0;
     function showBalanceChanges({nonce, data: balance}){
         console.log('free balance is ' + balance.free/DOT_DECIMAL_PLACES + " WND with " + balance.reserved + " reserved.");
         if(++balanceChangeCount == 5) balanceUnsub(); // balanceSub() 
     }
+
     const balanceUnsub = await api.query.system.account(ADDR, showBalanceChanges);
-
-
-    //Subscribe to balance changes for our account
-    // const unsub = await api.query.system.account(ADDR, ({ nonce, data: balance }) => {
-    //   console.log(`free balance is ${balance.free/DOT_DECIMAL_PLACES} westies with ${balance.reserved} reserved and a nonce of ${nonce}`);
-    // });
 
 })()
