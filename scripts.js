83df25be4367379802a68b999088c17311c9a43a
Matthew Hrydil <mhrydil@gmail.com>
2020-09-29 19:16:13 -0400

    Generate circle of chains based on a number given, and animate messages to/from different chains

--- scripts.js
+++ scripts.js
@@ -23,28 +23,63 @@ app.listen(port, () => {
 })
 
 */
-var path = '#path3'
-function showMessage(){
-    console.log("animating!");
-    anime({
-        targets: '#message_svg ' + path,
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'steps(30)',
-        duration: 5000,
-        // direction: 'alternate',
-        // loop: true
-    })
+
+function sendMessage() {
+    from_id = document.getElementById('from_chain').value;
+    to_id = document.getElementById('to_chain').value;
+    console.log("Animating sending a message from " + from_id + " to " + to_id);
+    for(i=0; i<3; i++){
+        anime({
+            targets: '#path_id_' + from_id,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'linear',
+            duration: 4000,
+            // direction: 'alternate',
+            // loop: true
+        })
+        anime({
+            targets: '#path_id_' + to_id,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'linear',
+            duration: 4000,
+            delay: 4000,
+            direction: 'reverse'
+            // loop: true
+        })
+    }
+    setTimeout(() => {generateCircles()}, 2000); //reset the paths after a message is sent... Not really sure why 4000 is the delay, I feel like it should be 8000 but idk
 }
 
+function generateCircles(){
+    console.log("Generating circles.");
+    elem = document.getElementById('message_svg');
+    number = document.getElementById('num_chains').value;
+    angleBetween = (360/number)*(Math.PI/180);
+    centerX = 400;
+    centerY = 400;
+    text = "";
+    for(var i=0; i<number; i++){
+        thisX = centerX + Math.cos(angleBetween*i)*325;
+        //50 + i*30;// x position for the ith circle
+        thisY = centerY + Math.sin(angleBetween*i)*325;
+        //40 + i*30;// y position for the ith circle
+        text += "<circle id='circle_id_" + i + "' cx='" + thisX + "' cy='" + thisY + "' r='40' stroke='black' stroke-width='3' fill='#EEEEEE' />\n";
+        text += "<path id='path_id_" + i + "'d='M" + thisX + " " + thisY + "L400 400 Z' stroke='black' stroke-width='3' />\n";
+        text += "<text x='" + (thisX-15) + "' y='" + (thisY-50) + "' fill='black'> ID: " + i + "</text>";
+        //<!-- <path id="path3" d="M75 125 L325 125 Z" stroke="black" stroke-width="3" /> -->
+        //<circle id="left_circle" cx="50" cy="125" r="40" stroke="black" stroke-width="3" fill="red" />
+    }
+    console.log(text);
+    elem.innerHTML = text;
+}
 
-circle_to_animate = '#left_circle';
 
 function showNewBlock(){
     console.log("animating the bottom left circle");
     anime({
-        targets: '#block_svg ' + circle_to_animate,
-        scale: .5,
-        duration: 1000,
+        targets: '#block_svg #left_circle',
+        scale: 1.25,
+        duration: 500,
         direction: 'alternate',
     })
 }
@@ -92,4 +127,4 @@ async function update_parachain_heads(api) {
     });
 }
 
-main()
+// main()
