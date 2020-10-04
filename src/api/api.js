6f4817c46ccffc4828d94b84977d7e69e2dfb61c
Roman Kasparian <roman.kasparian@gmail.com>
2020-10-03 20:35:30 -0400

    fixed object references

--- src/api/api.js
+++ src/api/api.js
@@ -4,12 +4,8 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 const uri = process.env.URI;
 
 export default class API {
-    API() {
-        console.log("loaded API class")
-    }
-
     papi = null;
-    ids = [];
+    ids = null;
     testMessage = 'ok';
 
     // test method to ensure api is working
@@ -20,27 +16,22 @@ export default class API {
     loadAPI() {
         return new Promise((resolve, reject) => {
             let provider = new WsProvider(uri);
-            console.log(provider);
             ApiPromise.create({ provider }).then((r) => {
                 this.papi = r;
                 this.papi.rpc.system.chain().then((r) => {
-
                     resolve(`Connected to ${r} at ${uri}`, r, uri);
-
-                }).catch((e) => { console.log("first catch block"); reject(e); });
-
-            }).catch((e) => { console.log("second catch block"); console.log(e); reject(e); });
+                }).catch((e) => { reject(e); });
+            }).catch((e) => { reject(e); });
         });
     }
 
-    // save parachain ids in ids
+    // save parachain ids in this.ids
     getParachainIDs() {
         return new Promise((resolve, reject) => {
-            if (this.papi == null) reject("API not loaded. Call loadAPI() before calling another function.");
+            if (this.papi == null) reject('API not loaded. Call loadAPI() before calling another function.');
             this.papi.query.registrar.parachains().then((r) => {
                 this.ids = r;
-                console.log(this.ids);
-                resolve(r);
+                resolve({ ids: r });
             }).catch((e) => { reject(e); })
         });
     }
@@ -48,20 +39,15 @@ export default class API {
     // return all parachains on relay
     getParachains() {
         return new Promise((resolve, reject) => {
-            if (this.papi == null) reject("API not loaded. Call loadAPI() before calling another function.");
+            if (this.papi == null) reject('API not loaded. Call loadAPI() before calling another function.');
             let headRequests = [];
-            console.log("before promise.all");
-            console.log(this.ids.length);
-            if (this.ids.length == 0) { reject('No parachain ids.'); }
+            if (this.ids == null || this.ids.length == 0) { reject('No parachain ids.'); }
             this.ids.forEach(id => {
-                console.log(id);
-                headRequests.push(papi.query.parachains.heads(id));
+                headRequests.push(this.papi.query.parachains.heads(id));
             });
-
             Promise.all(headRequests).then((heads) => {
                 let response = {};
                 response.parachains = [];
-                console.log(headRequests.length);
                 for (let i = 0; i < headRequests.length; i++) {
                     response.parachains.push({
                         id: this.ids[i],
@@ -72,7 +58,6 @@ export default class API {
                 resolve(response);
             }).catch((e) => {
                 // Parachain head request failed
-                console.log(e);
                 reject(e);
             });
         });
